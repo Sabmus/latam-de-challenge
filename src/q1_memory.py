978e11c190dd9f8203527ea1dea950bdf2a0eebd
sabmus <abiatar.ms@gmail.com>
2024-07-03 09:46:32 -0400

    feat: add q1_memory initial implementation

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,16 @@
 from typing import List, Tuple
 from datetime import datetime
+from spark_class import SparkClass
+from memory_profiler import profile
 
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    # Inicializacion de Spark
+    spark = SparkClass("Q1: Memory")
+    # Carga de datos
+    json_data = spark.load_json(file_path)
+
+    
+    # termino ejecucion de spark
+    spark.stop()
+    return []
