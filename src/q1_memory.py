6dc668a462371a6544d700831c07880e4c7baa63
Sabmus <abiatar.ms@gmail.com>
2024-07-03 14:12:01 -0400

    refactor: change pyspark functions import

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from spark_class import SparkClass
 from memory_profiler import profile
 from pyspark.sql.types import DateType
-from pyspark.sql.functions import desc, count
+from pyspark.sql import functions as sf 
 
 @profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
@@ -11,11 +11,12 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     spark = SparkClass("Q1: Memory")
     # Carga de datos
     json_data = spark.load_json(file_path)
+
+    # TODO: ARREGLAR ESTO
     quoted1 = json_data.select("quotedTweet.*")
     quoted2 = quoted1.select("quotedTweet.*")
     quoted3 = quoted2.select("quotedTweet.*").select("date", "id", "user.username")
 
-    # TODO: ARREGLAR ESTO
     df = json_data.select("date", "id", "user.username") \
         .union(quoted1.select("date", "id", "user.username")) \
         .union(quoted2.select("date", "id", "user.username")) \
@@ -24,21 +25,22 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
 
     # obtengo las top 10 fechas con mas tweets
     top_10_dates = df.groupBy(df["date"].cast(DateType()).alias("date")) \
-        .agg(count("id").alias("tweetCount")) \
-        .orderBy(desc("tweetCount")) \
+        .agg(sf.count("id").alias("tweetCount")) \
+        .orderBy(sf.desc("tweetCount")) \
         .limit(10)
     
     # filtro el df principal con las top 10 fechas para luego agrupar según cantidad de tweets por usuario
     top_user_by_date = df.filter(df.date.cast(DateType()).isin([row.date for row in top_10_dates.collect()])) \
         .groupBy(df["date"].cast(DateType()).alias("date"), "username") \
-        .agg(count("id").alias("tweetCount")).orderBy(desc("tweetCount")) \
+        .agg(sf.count("id").alias("tweetCount")).orderBy(sf.desc("tweetCount")) \
         .limit(10)
     
     # hago un join con ambos df para obtener el resultado final
     result = top_10_dates.join(top_user_by_date, 
                top_10_dates.date == top_user_by_date.date, 
-               "inner").select(top_10_dates.date, top_user_by_date.username).orderBy(desc(top_10_dates.tweetCount)).collect()
+               "inner").select(top_10_dates.date, top_user_by_date.username).orderBy(sf.desc(top_10_dates.tweetCount)).collect()
 
     # termino ejecucion de spark
+    spark.get_spark().catalog.clearCache()
     spark.stop()
     return [(row.date, row.username) for row in result]
