8705e194ef950e1e741db629814dd1431474d3d3
sabmus <abiatar.ms@gmail.com>
2024-07-05 17:32:40 -0400

    perf: improve performance by reducing content column size
    with short_pattern content column was reduced eliminating almost every non emoji character

--- src/q2_time.py
+++ src/q2_time.py
@@ -9,28 +9,34 @@ def q2_time(file_path: str) -> List[Tuple[str, int]]:
     spark = SparkClass("Q2: Time")
 
     # Carga de datos
-    df = spark.load_parquet(file_path).select("content").cache()
+    df = spark.load_parquet(file_path).select("content").where(sf.col("content").isNotNull()).cache()
 
+    short_pattern = '([\u0900-\u097F]+|[\x00-\x7F]+)'
+    content = df.select(sf.regexp_replace(sf.col('content'), fr'{short_pattern}', '').alias("content")) \
+        .filter(sf.col("content") != "").cache()
+    
+    df.unpersist()
+    
     emojis = sorted(emoji.EMOJI_DATA, key=len, reverse=True) 
     pattern = '(' + '|'.join(re.escape(u) for u in emojis) + ')'
- 
+
     # Extract emojis using regexp_extract
-    df2 = df.withColumn("emojis", sf.expr(f"regexp_extract_all(content, r'{pattern}')")) \
+    df2 = content.withColumn("emojis", sf.expr(f"regexp_extract_all(content, r'{pattern}')")) \
         .where(sf.size(sf.col("emojis")) > 0) \
         .drop("content") \
         .cache()
     
-    print(df2.printSchema())
-
+    content.unpersist()
+    
     df3 = df2.select(sf.explode("emojis").alias("emoji")).cache()
 
+    df2.unpersist()
+    
     top_10_emojis = df3.groupBy("emoji") \
         .agg(sf.count("emoji").alias("emojiCount")) \
         .orderBy(sf.desc("emojiCount")) \
         .take(10)
     
-    df.unpersist()
-    df2.unpersist()
     df3.unpersist()
 
     # termino ejecucion de spark
