71bd91e8cd9a79e903fc799044da92a69374ab0c
sabmus <abiatar.ms@gmail.com>
2024-07-05 12:56:30 -0400

    refactor: create variable names for configuration fields

--- src/spark_class.py
+++ src/spark_class.py
@@ -7,9 +7,16 @@ executor_cores: int = 2
 executor_memory: str = "4g"
 driver_cores: int = 4
 driver_memory: str = "4g"
+# max_partition_bytes
+# 38 mb tomando en cuenta el peso del archivo parquet, partido en 4 particiones
 max_partition_bytes: int = (38 * 1024 * 1024) // 4
 maxResultSize: str = "1g"
 memoryOverhead: str = "1g"
+network_timeout: str = "240s"
+network_timeoutInterval: str = "120s"
+sql_files_minPartitionNum: int = 4
+sql_debu_maxToStringFields: int = 50
+parquet_binaryAsString: bool = True
 
 class SparkClass:
     def __init__(self, app_name: str, master: str = master, executor_cores: int = executor_cores, executor_memory: str = executor_memory, max_partition_bytes: int = max_partition_bytes):
@@ -25,14 +32,12 @@ class SparkClass:
             .set("spark.driver.maxResultSize", maxResultSize) \
             .set("spark.sql.files.maxPartitionBytes", max_partition_bytes) \
             .set("spark.files.maxPartitionBytes", max_partition_bytes) \
-            .set("spark.network.timeout", "240s") \
-            .set("spark.network.timeoutInterval", "120s") \
-            .set("spark.sql.files.minPartitionNum", 4) \
-            .set("spark.sql.debug.maxToStringFields", 50) \
-            .set("spark.sql.parquet.binaryAsString", True) \
-            #.set("spark.sql.analyzer.failAmbiguousSelfJoin", False) \
+            .set("spark.network.timeout", network_timeout) \
+            .set("spark.network.timeoutInterval", network_timeoutInterval) \
+            .set("spark.sql.files.minPartitionNum", sql_files_minPartitionNum) \
+            .set("spark.sql.debug.maxToStringFields", sql_debu_maxToStringFields) \
+            .set("spark.sql.parquet.binaryAsString", parquet_binaryAsString) \
             
-
         self.spark = SparkSession.builder.config(conf=conf).getOrCreate()
     
     def load_json(self, file_path: str):
